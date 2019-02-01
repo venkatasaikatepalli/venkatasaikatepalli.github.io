8d74d19b1a2b7ca571810990541667589ac86762
venkatasaikatepalli <venkatasaikatepalli@gmail.com>
2019-02-01 10:41:56 +0530

    updated profile to new version

---
+++ js/app.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    var i = 0;
+    var txt = 'Hi, I am';
+    var speed = 120;
+
+    function typeWriter() {
+        if (i < txt.length) {
+            document.getElementById("m-auth").innerHTML += txt.charAt(i);
+            i++;
+            setTimeout(typeWriter, speed);
+        }
+    }
+    typeWriter();
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        var hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body, .col-md-9').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function(){
+    
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+        });
+        } // End if
+    });
+});
